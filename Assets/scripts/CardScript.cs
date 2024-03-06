3de9d992d64053aea8c2d813e831e527f72669f1
Pichupid0 <valasenko30.ilja@gmail.com>
2024-03-06 19:39:12 +0000

    Cards and Deck Scripts

---
+++ Assets/scripts/CardScript.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardScript : MonoBehaviour
+{
+    [SerializeField] string region; // region name
+    [SerializeField] string troopsType; // what type of troops are on the card
+
+    //as i understand there are two type of cards wild and territory
+    [SerializeField] string cardType;
+
+}
